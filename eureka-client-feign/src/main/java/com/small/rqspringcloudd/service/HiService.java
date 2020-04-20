eadf8ed7a13f48669758d9d1d36170ebee1f8d8d
ranq <751016301@qq.com>
2020-04-20 17:30:13 +0800

    ’接口分离出来‘

--- eureka-client-feign/src/main/java/com/small/rqspringcloudd/service/HiService.java
+++ eureka-client-feign/src/main/java/com/small/rqspringcloudd/service/HiService.java
@@ -5,9 +5,8 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import service.HiCommonService;
 
 @FeignClient(value = "eurekaclientbase", fallback = HiServiceCallback.class)
-public interface HiService {
-    @RequestMapping(value = "/hi", method = RequestMethod.GET)
-    String sayHi(@RequestParam(value = "name") String name);
+public interface HiService extends HiCommonService {
 }
